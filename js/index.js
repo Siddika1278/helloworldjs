04401b0d6496e4e450939f43fbb3119b90d7e97f
Siddika Ajmeri <saajmeri@gmail.com>
2023-06-09 17:58:13 +0530

    commit 6-9-23

--- js/index.js
+++ js/index.js
@@ -13,3 +13,18 @@ function updateName() {
   const name = prompt("Enter a new name");
   para.textContent = `Player 1: ${name}`;
 }
+
+//fetch api 
+async function logJSONData(){
+  try{
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    
+    const result = await response.json();
+    document.getElementById('display_div').innerHTML = JSON.stringify(result);
+    console.log(result)
+  }
+  catch(error){
+    console.error("Error: ", error);
+  }
+}
+logJSONData();
