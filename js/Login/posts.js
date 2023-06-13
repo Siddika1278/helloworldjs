22b21bd59f52854a8a06b9cf5a54e5cecf3f4bf2
Siddika Ajmeri <saajmeri@gmail.com>
2023-06-13 10:14:21 +0530

    final commit of the day

--- js/Login/posts.js
+++ js/Login/posts.js
@@ -6,11 +6,20 @@ async function logJSONData( url){
       
       const result = await response.json();
       document.getElementById('fetch').innerHTML = JSON.stringify(result);
-      console.log(result)
+      console.log(result);
+      let html = "";
+      result.forEach((post)=>{
+        html += '<div>'+
+                  '<p>' +post.id +'</p>'+
+                  '<p>'+ post.body +'</p>'+
+                '</div>';
+      });
+      document.getElementById('fetch').innerHTML = html;
+        
     }
     catch(error){
       console.error("Error: ", error);
     }
   }
-  logJSONData("https://jsonplaceholder.typicode.com/posts");
+  logJSONData("https://jsonplaceholder.typicode.com/posts"); 
   
