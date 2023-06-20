487ea06e3f56263de383b95c6e7fecddb076f62f
Siddika Ajmeri <saajmeri@gmail.com>
2023-06-20 16:16:06 +0530

    Done

--- js/ExpenseTracker/firstpage.js
+++ js/ExpenseTracker/firstpage.js
@@ -34,7 +34,7 @@ function add() {
     // console.log(expense.date, expense.time, expense.category, expense.amount, expense.desription, expense.method);
     console.log(expenses);
    
-    let html = "";
+    // let html = "";
 
 //Here it loops for aal elements so for large data it'll take time to load so instead we have to append
     // expenses.forEach((variable)=>{
@@ -66,11 +66,24 @@ document.getElementById('data').innerHTML +=
     `<li class="eachentry" expid="${expenses.length -1}" onclick="details('${expenses.length -1}')">
     `+getBody(variable)+`</li>
     ` ;
+reset();
+
 }
 function getBody(m){
     return `<div class="title">${m.category}</div>
     <div class="amount">Rs. ${m.amount}</div>
     <div class="description">${m.description}</div>`;
+}
+//that will reset the values of formfields to null
+//call this function in add and updateDetails
+function reset(){
+    document.getElementById('date').value = "";
+    document.getElementById('time').value = "";
+    document.getElementById('category').value = "";
+    document.getElementById('amount').value = "";
+    document.getElementById('description').value = "";
+    document.querySelector('input[name="method"]:checked').checked = false
+
 }
 
 function details(x){
@@ -80,8 +93,9 @@ function details(x){
     document.getElementById('category').value = singleexp.category;
     document.getElementById('amount').value = singleexp.amount;
     document.getElementById('description').value = singleexp.description;
-    document.querySelector('input[value="'+singleexp.method+'"]').click();
+    document.querySelector('input[value="'+singleexp.method+'"]').checked = true;
 
+    //hiding add button ada showning update button
     document.getElementById('addbtn').classList.add('hidden');
     document.getElementById('update').classList.remove('hidden');
     document.getElementById('update').setAttribute("expid",x);
@@ -95,7 +109,12 @@ function updateDetails(){
     expenses[x].amount = document.getElementById('amount').value,
     expenses[x].description = document.getElementById('description').value,
     expenses[x].method = document.querySelector('input[name ="method"]:checked').value;
-
+    //showing the updated values in right side list
+    //for the id that we updated the values in li we change it , i.e called the function and it takes the new values using id x
     document.querySelector('li[expid="'+x+'"]').innerHTML = getBody(expenses[x]);
+    //reseting the values and showing add button and hiding update button
+    reset();
+    document.getElementById('update').classList.add('hidden');
+    document.getElementById('addbtn').classList.remove('hidden');
 }
 
