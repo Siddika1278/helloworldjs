7cee52d1c29457aea2bf952809365b4a6ae6711f
Siddika Ajmeri <saajmeri@gmail.com>
2023-06-22 01:40:43 +0530

    form validation

--- js/ExpenseTracker/firstpage.js
+++ js/ExpenseTracker/firstpage.js
@@ -17,7 +17,8 @@ function add() {
     // document.querySelector('input[name="method"]:checked').value 
     // );
 
-
+    // validate();
+    if(!validate()) return false;
 //2. Stroing the input values in object
    
     const singleExp = {
@@ -61,20 +62,46 @@ function add() {
 // }
 
 
+// //1. adding id to the object
+// expenses[expenses.length -1].id = expenses.length -1;
+// //setting variable with new object that has id
+// variable = expenses[expenses.length -1];
+// document.getElementById('data').innerHTML += 
+//     `<li class="eachentry" expid="${expenses.length -1}" onclick="details('${expenses.length -1}')">
+//     `+getBody(variable)+ `</li>
+//     ` ;
+// reset();
+// }
+// function getBody(m){
+//     return `<div class="title">${m.category}</div>
+//     <div class="amount">Rs. ${m.amount}</div>
+//     <div class="description">${m.description}</div>
+//     <button onclick="deleteelem('${m.id}')">DELETE</button>
+//     `;
+//     //m.id gives the id
+// }
+
+
+//2. using another parameter
+
 variable = singleExp;
 document.getElementById('data').innerHTML += 
-    `<li class="eachentry" expid="${expenses.length -1}" onclick="details('${expenses.length -1}')">
-    `+getBody(variable)+ `<button onclick="deleteelem('${expenses.length -1}')">DELETE</button></li>
-    ` ;
+`<li class="eachentry" expid="${expenses.length -1}" onclick="details('${expenses.length -1}')">
+`+getBody(variable,expenses.length -1)+ `</li>
+` ;
 reset();
+}
 
+function getBody(m,s){
+return `<div class="title">${m.category}</div>
+<div class="amount">Rs. ${m.amount}</div>
+<div class="description">${m.description}</div>
+<button class="deletebutton" onclick="deleteelem('${s}')">DELETE</button>
+`;
 }
-function getBody(m){
-    return `<div class="title">${m.category}</div>
-    <div class="amount">Rs. ${m.amount}</div>
-    <div class="description">${m.description}</div>
-    `;
-}
+
+
+
 //that will reset the values of formfields to null
 //call this function in add and updateDetails
 function reset(){
@@ -97,7 +124,11 @@ function details(x){
     document.getElementById('description').value = singleexp.description;
     document.querySelector('input[value="'+singleexp.method+'"]').checked = true;
 
-    //hiding add button ada showning update button
+    //Showing the edit title
+    document.getElementById('insert').classList.add('hidden');
+    document.getElementById('edit').classList.remove('hidden');
+
+    //hiding add button ana showning update button
     document.getElementById('addbtn').classList.add('hidden');
     document.getElementById('update').classList.remove('hidden');
     document.getElementById('update').setAttribute("expid",x);
@@ -113,9 +144,18 @@ function updateDetails(){
     expenses[x].method = document.querySelector('input[name ="method"]:checked').value;
     //showing the updated values in right side list
     //for the id that we updated the values in li we change it , i.e called the function and it takes the new values using id x
-    document.querySelector('li[expid="'+x+'"]').innerHTML = getBody(expenses[x]);
+    
+    //when not taking another parameter
+    // document.querySelector('li[expid="'+x+'"]').innerHTML = getBody(expenses[x]);
+
+    //when using another parameter
+    document.querySelector('li[expid="'+x+'"]').innerHTML = getBody(expenses[x],x);
     //reseting the values and showing add button and hiding update button
     reset();
+
+    document.getElementById('insert').classList.remove('hidden');
+    document.getElementById('edit').classList.add('hidden');
+
     document.getElementById('update').classList.add('hidden');
     document.getElementById('addbtn').classList.remove('hidden');
 }
@@ -134,7 +174,75 @@ function deleteelem(ms){
 //event.default/stop propogation oe etc
 
 //li ki detail wali method , event bubble ho ra hai jab delete click kiya hau , if condition expence[x]=undefined =>return true
-//
-
 
 //validations: every field required => if not inserted it should show error
+
+function validate(){
+    // if(document.getElementById('date').value == ""){
+    //     // alert("Enter the date!");
+    //     document.getElementById('datealert').innerHTML = 
+    //     `You need to enter the date` ;
+    //     return false;
+    // }
+    // else
+    //     document.getElementById('datealert').innerHTML = '';
+    
+
+    if(document.getElementById('date').value == ""){
+        // alert("Enter the time");
+        document.getElementById('datelabel').classList.add('error');
+        return false;
+    }
+    else document.getElementById('datelabel').classList.remove('error');
+
+
+    if(document.getElementById('time').value == ""){
+        // alert("Enter the time");
+        document.getElementById('timelabel').classList.add('error');
+        return false;
+    }
+    else document.getElementById('timelabel').classList.remove('error');
+    // if(document.getElementById('category').value == "Choose a Category"){
+    //     alert("Choose a Category");
+    // }
+    // if(document.getElementById('amount').value == ""){
+    //     alert("Enter the amount");
+    // }
+    // if(document.getElementById('description').value == ""){
+    //     alert("Enter the description");
+    // }
+    // // if(document.querySelector('input[name="method"]:checked') == null){
+    // //     alert("Select mode of payment");
+    // // }   
+    // if(!document.querySelectorAll('input[name="method"]:checked').length){
+    //     alert("Select mode of payment");
+    // }
+    if(document.getElementById('category').value == "Choose a Category"){
+        document.getElementById('categorylabel').classList.add('error');
+        return false;
+    }
+    else document.getElementById('categorylabel').classList.remove('error');
+    
+    if(document.getElementById('amount').value == ""){
+        document.getElementById('amountlabel').classList.add('error');
+        return false;
+    }
+    else document.getElementById('amountlabel').classList.remove('error');
+    
+    if(document.getElementById('description').value == ""){
+        document.getElementById('descriptionlabel').classList.add('error');
+        return false;
+    }
+    else document.getElementById('descriptionlabel').classList.remove('error');
+    
+    // if(document.querySelector('input[name="method"]:checked') == null){
+    //     alert("Select mode of payment");
+    // }   
+    if(!document.querySelectorAll('input[name="method"]:checked').length){
+        document.getElementById('paymentdiv').classList.add('error');
+        return false;
+    }
+    else document.getElementById('paymentdiv').classList.remove('error');
+    
+    return true;
+}   
