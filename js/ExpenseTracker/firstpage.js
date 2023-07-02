c18896f3587963e54232a936b9ba456855dcc1b3
Siddika Ajmeri <saajmeri@gmail.com>
2023-07-02 17:10:35 +0530

    clean code

--- js/ExpenseTracker/firstpage.js
+++ js/ExpenseTracker/firstpage.js
@@ -1,191 +1,4 @@
-//Pseudo Code
-// expenses = []
-// on each add
-// singleEXp = {date:,amount,}
-// exp.add(singleEXp);
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// import firebase from "firebase/app";
-// import "firebase/firestore";
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDWcpSJyDXwh6ReQ_8DvnIimkswPUz1GV0",
-//   authDomain: "expensetracker-25411.firebaseapp.com",
-//   projectId: "expensetracker-25411",
-//   storageBucket: "expensetracker-25411.appspot.com",
-//   messagingSenderId: "109718325029",
-//   appId: "1:109718325029:web:c4cfa6062340fc60d57ccc"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// //Initialize Cloud Firestore and get a reference to the service
-// const db = firebase.firestore();
-
-// db.collection("testing").add({
-//    first: "Ada",
-//    last: "Lovelace",
-//    born:1815
-// })
-// .then((docRef) => {
-//    console.log("Document written with ID: ", docRef.id);
-// })
-// .catch((error) => {
-//    console.error("Error adding document: ", error);
-// });
-
 expenses = [];
-// expenses = [
-//     {
-//         "date": "2023-06-19",
-//         "time": "17:01",
-//         "category": "Dairy Products",
-//         "amount": "9576",
-//         "description": "ews",
-//         "method": "cash"
-//     },
-//     {
-//         "date": "2023-06-23",
-//         "time": "17:02",
-//         "category": "Snacks",
-//         "amount": "900",
-//         "description": "ews",
-//         "method": "cash"
-//     },
-//     {
-//         "date": "2023-06-19",
-//         "time": "17:32",
-//         "category": "Electronics",
-//         "amount": "78542",
-//         "description": "ews",
-//         "method": "cash"
-//     },
-//     {
-//         "date": "2023-06-19",
-//         "time": "17:42",
-//         "category": "Travel Expense",
-//         "amount": "78965",
-//         "description": "ews",
-//         "method": "cash"
-//     },
-//     {
-//         "date": "2023-06-19",
-//         "time": "17:42",
-//         "category": "Snacks",
-//         "amount": "530",
-//         "description": "ews",
-//         "method": "cash"
-//     },
-//     {
-//         "date": "2023-06-19",
-//         "time": "17:42",
-//         "category": "Dairy Products",
-//         "amount": "856",
-//         "description": "ews",
-//         "method": "cash"
-//     },
-//     {
-//         "date": "2023-06-19",
-//         "time": "17:42",
-//         "category": "Electronics",
-//         "amount": "23564",
-//         "description": "ews",
-//         "method": "cash"
-//     }
-// ];
-
-//Comenting to check the add function that would work for firestore
-
-// function add() {
-//     //1. Oncick of add button the function prints the inputed value to the console.
-
-//     // console.log(document.getElementById('date').value,
-//     // document.getElementById('time').value,
-//     // document.getElementById('category').value,
-//     // document.getElementById('amount').value,
-//     // document.getElementById('description').value,
-//     // document.querySelector('input[name="method"]:checked').value 
-//     // );
-
-//     // validate();
-//     if (!validate()) return false;
-//     //2. Stroing the input values in object
-
-//     const singleExp = {
-//         date: document.getElementById('date').value,
-//         time: document.getElementById('time').value,
-//         category: document.getElementById('category').value,
-//         amount: document.getElementById('amount').value,
-//         description: document.getElementById('description').value,
-//         method: document.querySelector('input[name="method"]:checked').value
-//     }
-//     //to add object to array
-//     expenses.push(singleExp);
-
-//     // console.log(expense.date, expense.time, expense.category, expense.amount, expense.desription, expense.method);
-//     console.log(expenses);
-
-//     // let html = "";
-
-//     //Here it loops for aal elements so for large data it'll take time to load so instead we have to append
-//     // expenses.forEach((variable)=>{
-
-//     // html+= `<tr><td>${variable.date}</td>
-//     // <td>${variable.time}</td>
-//     // <td>${variable.category}</td>
-//     // <td>${variable.amount}</td>
-//     // <td>${variable.description}</td> 
-//     // <td>${variable.method}</td> 
-//     // <tr>`});
-
-
-//     //here we append new row with input data in tablebody in html file we gave id="data" in tbody tag
-//     // variable = singleExp;
-//     // document.getElementById('data').innerHTML += 
-//     //     `<tr><td>${variable.date}</td>
-//     //     <td>${variable.time}</td>
-//     //     <td>${variable.category}</td>
-//     //     <td>${variable.amount}</td>
-//     //     <td>${variable.description}</td> 
-//     //     <td>${variable.method}</td> 
-//     //     <tr>` ;
-//     // }
-
-
-//     // //1. adding id to the object
-//     // expenses[expenses.length -1].id = expenses.length -1;
-//     // //setting variable with new object that has id
-//     // variable = expenses[expenses.length -1];
-//     // document.getElementById('data').innerHTML += 
-//     //     `<li class="eachentry" expid="${expenses.length -1}" onclick="details('${expenses.length -1}')">
-//     //     `+getBody(variable)+ `</li>
-//     //     ` ;
-//     // resetThis();
-//     // }
-//     // function getBody(m){
-//     //     return `<div class="title">${m.category}</div>
-//     //     <div class="amount">Rs. ${m.amount}</div>
-//     //     <div class="description">${m.description}</div>
-//     //     <button onclick="deleteelem('${m.id}')">DELETE</button>
-//     //     `;
-//     //     //m.id gives the id
-//     // }
-
-
-//     //2. using another parameter
-
-//     variable = singleExp;
-//     document.getElementById('data').innerHTML +=
-//         `<li class="eachentry">
-// `+ getBody(variable, expenses.length - 1) + `</li>
-// ` ;
-// resetThis();
-// }
 
 function getBody(m, s) {
     return `<div class="title">${m.category}</div>
@@ -200,67 +13,18 @@ function getBody(m, s) {
 <button class="deletebutton" expid="${s}">DELETE</button></div>
 `;
 }
-//before change 
-// <button class="viewbutton" onclick="viewelem('${s}')">VIEW</button> 
-//<button class="editbutton" expid="${s}" onclick="details('${s}')">EDIT</button>
-//<button class="deletebutton" onclick="deleteelem('${s}')">DELETE</button></div>
 
-
-//orignal one
-// function getBody(m, s) {
-//     return `<div class="title">${m.category}</div>
-// <div class="amount">Rs. ${m.amount}</div>
-// <div  class="description">${m.description}</div>
-
-// <div class="buttonsdesign">
-// <button class="viewbutton" expid="${s}">VIEW</button>
-// <button class="editbutton" expid="${s}">EDIT</button>
-// <button class="deletebutton" expid="${s}">DELETE</button></div>
-// `;
-// }
-
-// function viewelem(xyz) {
-//     document.getElementById('formcontent').classList.add('hidden');
-//     document.getElementById('edit').classList.remove('hidden');
-//     //    let v=Object.values(xyz);
-//     let obj = expenses[xyz];
-//     // for (i = 0; i < k.length; i++) {
-//     //    document.getElementById('tablee').innerHTML += ` <tr>
-//     //         <td>${k[i]}</td>     
-//     //         <td>${}</td>      
-//     //     </tr>`
-//     //     // document.getElementById('property').innerHTML += `${k[i]}`
-//     //     // document.getElementById('value').innerHTML += `${xyz[k[i]]}`
-//     // }
-//     let html = "";
-//     Object.entries(obj).forEach(arr => {
-//         html += ` <tr>
-//             <td>${arr[0]}</td>     
-//             <td>${arr[1]}</td>      
-//         </tr>`;
-//     });
-//     document.getElementById('tbody').innerHTML = html;
-
-
-// }
 //cross button on view
 function change() {
     document.getElementById('edit').classList.add('hidden');
     document.getElementById('formcontent').classList.remove('hidden');
     resetThis();
 }
-
-//that will reset the values of formfields to null
-//call this function in add and updateDetails
-// import {getSingleDoc} from "/js/ExpenseTracker/firstpageFB.js"; 
-
 //changing function details(x) , getting whole object in here so we don't have to export-import and by that we wont get id so getting 
 //id through parameter
 async function details(elemdata,x) {
     //changing title
-    // document.getElementById('inserttitle').innerHTML = `Edit Your Transaction`;
     document.getElementById('inserttitle').innerHTML = `Edit Your `;
-    
     // let singleexp = await getSingleDoc(x);
     let singleexp =  elemdata;
     // let singleexp = expenses[x];
@@ -272,19 +36,13 @@ async function details(elemdata,x) {
     document.getElementById('description').value = singleexp.description;
     document.querySelector('input[value="' + singleexp.method + '"]').checked = true;
 
-    //Showing the edit title
-    // document.getElementById('inserttitle').classList.add('hidden');
-    // document.getElementById('edit').classList.remove('hidden');
-
     //hiding add button ana showning update button
-
     document.getElementById('addbtn').classList.add('hidden');
     document.getElementById('update').classList.remove('hidden');
     document.getElementById('update').setAttribute("expid", x);
 }
 
 function resetThis() {
-    // document.getElementById('inserttitle').innerHTML = `New Transaction`;
     document.getElementById('inserttitle').innerHTML = `New `;
     document.getElementById('date').value = "";
     document.getElementById('time').value = "";
@@ -299,67 +57,7 @@ function resetThis() {
 
 }
 
-// function updateDetails() {
-//     let x = document.getElementById('update').getAttribute('expid');
-//     expenses[x].date = document.getElementById('date').value,
-//         expenses[x].time = document.getElementById('time').value,
-//         expenses[x].category = document.getElementById('category').value,
-//         expenses[x].amount = document.getElementById('amount').value,
-//         expenses[x].description = document.getElementById('description').value,
-//         expenses[x].method = document.querySelector('input[name ="method"]:checked').value;
-//     //showing the updated values in right side list
-//     //for the id that we updated the values in li we change it , i.e called the function and it takes the new values using id x
-
-//     //when not taking another parameter
-//     // document.querySelector('li[expid="'+x+'"]').innerHTML = getBody(expenses[x]);
-
-//     //when using another parameter
-//     // document.querySelector('li[expid="' + x + '"]').innerHTML = getBody(expenses[x], x);
-//     document.querySelector('li[expid="' + x + '"]').innerHTML = getBody(expenses[x], x);
-
-//     //reseting the values and showing add button and hiding update button
-//     resetThis();
-
-//     // document.getElementById('inserttitle').classList.remove('hidden');
-//     // document.getElementById('edit').classList.add('hidden');
-// }
-
-
-
-// function deleteelem(ms) {
-//     //we set it undefined as we wanted to remove the details but wanted to preserve the index
-//     expenses[ms] = undefined;
-//     document.querySelector('li[expid="' + ms + '"]').remove();
-
-//     if (document.getElementById('update').getAttribute('expid') == ms) {
-//         resetThis();
-//     }
-// }
-
-
-//mdn properties see
-
-//delete button/a => on click => likedetais(x)=> delete(x/id)
-//expenses[] assign undefine to that id's element expenses[x]=undefined;
-//if we delete id then 
-//on delete button, from id we'll fetch data and remove li element
-//event.default/stop propogation oe etc
-
-//li ki detail wali method , event bubble ho ra hai jab delete click kiya hau , if condition expence[x]=undefined =>return true
-
-//validations: every field required => if not inserted it should show error
-
 function validate() {
-    // if(document.getElementById('date').value == ""){
-    //     // alert("Enter the date!");
-    //     document.getElementById('datealert').innerHTML = 
-    //     `You need to enter the date` ;
-    //     return false;
-    // }
-    // else
-    //     document.getElementById('datealert').innerHTML = '';
-
-
     if (document.getElementById('date').value == "") {
         // alert("Enter the time");
         document.getElementById('datelabel').classList.add('error');
@@ -374,21 +72,7 @@ function validate() {
         return false;
     }
     else document.getElementById('timelabel').classList.remove('error');
-    // if(document.getElementById('category').value == "Choose a Category"){
-    //     alert("Choose a Category");
-    // }
-    // if(document.getElementById('amount').value == ""){
-    //     alert("Enter the amount");
-    // }
-    // if(document.getElementById('description').value == ""){
-    //     alert("Enter the description");
-    // }
-    // // if(document.querySelector('input[name="method"]:checked') == null){
-    // //     alert("Select mode of payment");
-    // // }   
-    // if(!document.querySelectorAll('input[name="method"]:checked').length){
-    //     alert("Select mode of payment");
-    // }
+   
     if (document.getElementById('category').value == "Choose a Category") {
         document.getElementById('categorylabel').classList.add('error');
         return false;
--- js/ExpenseTracker/firstpageFB.js
+++ js/ExpenseTracker/firstpageFB.js
@@ -1,15 +1,4 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-// import firebase from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js";
-
-// import firebase/firestore;// from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// import firebase from "firebase/compat/app";
-// // Required for side-effects
-// import "firebase/firestore";
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
 import { getFirestore, collection, addDoc, getDoc, getDocs, doc, deleteDoc, updateDoc } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
 
@@ -29,41 +18,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 // console.log(expenses); 
 
-//code written for checking    
-// try {
-// const docRef = await addDoc(collection(db, "users"), {
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-// });
-// console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-// console.error("Error adding document: ", e);
-// }
-
-//To store data in our expenses arrary in the firestore collection entries
-
-// try {
-//     expenses.forEach(async element => {
-//         const docRef = await addDoc(collection(db, "entries"), {
-
-//             date: element.date,
-//             time: element.time,
-//             category: element.category,
-//             amount: element.amount,
-//             description: element.description,
-//             method: element.method
-//             });
-//             console.log("Document written with ID: ", docRef.id);
-//     });
-//     } 
-//     catch (e) {
-//     console.error("Error adding document: ", e);
-//     }
-
 //for each new entry storing data to firestore
-
-
 export async function addfb() {
     if (!validate()) return false;
     let id = '';
@@ -79,46 +34,12 @@ export async function addfb() {
             description: document.getElementById('description').value,
             method: document.querySelector('input[name="method"]:checked').value
         });
-        // console.log("added");
-        // id = docRef.id;
-        // console.log("Document written with ID: ", docRef.id);
-
     }
     catch (e) {
         console.error("Error adding document: ", e);
     }
-
     // console.log(id);
-
-    //     document.getElementById('data').innerHTML +=
-    //         `<li class="eachentry">
-    // `+ getBody(variable, docRef.id) + `</li>
-    // ` ;
-    // const docRef = doc(db, "cities", "2l3bcSGs2vZBIc3RODwp");
-    // const docSnap = await getDocs(collection(db,"entries",id));
-
-    //1
-    // try{
-    // const docReff =  doc(collection(db, 'entries'), id);
-    // const docSnap = await getDoc(docReff);
-    // console.log(docSnap.data());   
-    // const collectionRef = collection(db, 'entries');
-    // const docReff = doc(collectionRef, id);
-    // const docSnap = await getDocs(docReff);
-    // console.log(docSnap.data());
-
-    // }
-    // catch(e){
-    //     console.log(e);
-    // }
-    //2
-
-    // const collectionRef = collection(db, 'entries');
-    // const docReff = doc(collectionRef, id);
-    // const docSnap = await getDocs(docReff);
-    // console.log(docSnap.data());
     getAllDoc();
-
     resetThis();
 }
 
@@ -162,8 +83,6 @@ export async function updateDetails(id) {
     resetThis();
 }
 
-
-
 //function details, what we did for it is that we passed our object in the method we already had and by
 //doing so we won't get id, so we passed id as another parameter. 
 //By all this we didn't had to make another method and comment the previous one as we did in viewelem
@@ -190,13 +109,9 @@ export async function viewelem(xyz) {
 //function to get all docs
 export async function getAllDoc() {
     try {
-        // const q = query(collection(db, "cities"), where("capital", "==", true));
         let html = "";
         //gets all data
         const querySnapshot = await getDocs(collection(db, "entries"));
-        //one thing we can do is get snapshot array of objects and use array.filter on it and pass it to next loop that prints the list.
-        // querySnapshot = [{},{},{}];
-        // querySnapshot.filter()
 
         let searchText = document.getElementById('search').value;
         let searchCategory = document.getElementById('searchCat').value;
@@ -209,69 +124,29 @@ export async function getAllDoc() {
         //another option we have is we check the condition for all eements and show those who passed the condition
         querySnapshot.forEach((doc) => {
             //filter conditions
-            
             let docData = doc.data();
             // console.log(docData);
             if (!docData.description.includes(searchText)) {
                 // console.log(searchText,docData.description);
                 return true;
             }
-            // //cat
-            // if(searchCategory != 'Select a Category'){
-            //      if(!(docData.category == searchCategory)){
-            //         return true;
-            //     }
-            // }    
-
-            //this instead of nested if
-
             if (searchCategory != '' && !(docData.category == searchCategory)) {
                 return true;
             }
-            //type
             if (searchMode != '' && !(docData.method == searchMode)) {
-                // console.log(searchMode ,docData.method );
                 return true;
             }
-            //amount 10-20
-            // console.log(parseInt(lowerLimit) <= docData.amount <= parseInt(upperLimit)); //doesn't takt condition together like this
-            // console.log(lowerLimit <= docData.amount && docData.amount <= upperLimit); //gave results not we want so this
-            // console.log(parseInt(lowerLimitt) <= docData.amount , docData.amount <= parseInt(upperLimitt), lowerLimitt,docData.amount, upperLimitt, );
-            //parseInt because the values we get from document are always in string so to convert it in int
-            if (lowerLimit != '' && upperLimit != '' && !(parseInt(lowerLimit) <= parseInt(docData.amount) && parseInt(docData.amount)<= parseInt(upperLimit))) {
+           if (lowerLimit != '' && upperLimit != '' && !(parseInt(lowerLimit) <= parseInt(docData.amount) && parseInt(docData.amount)<= parseInt(upperLimit))) {
                 return true;
             }
-            //date
             if (startDate != '' && endDate != '' && !(new Date(startDate) <= new Date(docData.date) && new Date(docData.date) <= new Date(endDate))) {
                 return true;
             }
-            // if(Date(startDate)<= docData.date && docData.date<= Date(endDate)){
-            //     return true;
-            // }
-            // doc.data() is never undefined for query doc snapshots
-
-            //getting data
-            // console.log(doc.id, " => ", doc.data());
-            // console.log(doc.data().category)
-
-            //ecitcarp siht +=  =  + 
-
             html +=
                 `<li class="eachentry">
          `+ getBody(docData, doc.id) + `</li>`;
-            // getBody(doc.data(),doc.id) //before filters it was like this
-            //wrote this here this would work but writing this outside would be more optimal it rewrites again 
-            //and again and we want what we get in last so just make it write once that will engage the dome less as earlier
-            //you can also write this inside by some changes recall how (one hint html ='' inside loop)
-            //  document.getElementById('data').innerHTML = html;       
-        });
+           });
         document.getElementById('data').innerHTML = html;
-        //We wrote this in script tag in html file but we didn't get it as we cannot use eventlistner on elements 
-        //we don't have in browser. This will work here as at this point we'll get the element
-        //and we cannot have same id for multiple things, so using querySelectorAll on class name ans giving eventlistner to all
-        //event.target getAttribute expid will get us expid of that particular we want
-        //see other functions or stuff we get with event as event.target also you can console.log(event) to see info
-
         document.querySelectorAll(".viewbutton").forEach((elem) => {
             elem.addEventListener("click", function (event) {
                 let x = event.target.getAttribute('expid');
@@ -282,9 +157,6 @@ export async function getAllDoc() {
         document.querySelectorAll(".editbutton").forEach((elem => {
             elem.addEventListener("click", async function (event) {
                 let x = event.target.getAttribute('expid');
-                // details(x);
-                // console.log(await getsingleDoc(x));
-                //passing id in parameter as we won't get id in object
                 details(await getsingleDoc(x), x);
             })
         }));
@@ -301,38 +173,3 @@ export async function getAllDoc() {
         console.log(e);
     }
 }
-
-//getting id by .then  ,normally it'll not five id i.r give undefined so assessing it by .then or add async await
-//then you'll get id directly for understanding both recall example of train
-// export function addfb() {
-//     if (!validate()) return false;
-
-//     try {
-
-//         const docRef = addDoc(collection(db, "entries"), {
-
-//             date: document.getElementById('date').value,
-//             time: document.getElementById('time').value,
-//             category: document.getElementById('category').value,
-//             amount: document.getElementById('amount').value,
-//             description: document.getElementById('description').value,
-//             method: document.querySelector('input[name="method"]:checked').value
-//         });
-//         docRef.then((resp)=>{
-//             console.log(resp.id);
-//         });
-//         console.log("Document written with ID: ", docRef.id);
-
-//     }
-//     catch (e) {
-//         console.error("Error adding document: ", e);
-//     }
-//     // const variable = docRef;
-//     // document.getElementById('data').innerHTML +=
-//     //     `<li class="eachentry">
-//     // `+ getBody(variable, docRef.id) + `</li>
-//     // ` ;
-//     resetThis();
-// }
-
-// export default addfb;
